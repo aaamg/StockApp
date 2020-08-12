8d5eb205ece6b4428109d8bdc652b6f9776c8fc7
Andrew García <52712672+aaamg@users.noreply.github.com>
2020-08-12 12:28:30 -0400

    Infinite loop Text updates api call, not the submit button. But we are "working" more than before

--- src/App.js
+++ src/App.js
@@ -34,71 +34,79 @@ export default function App() {
     volume: ""
   });
 
-  useEffect(() => {
-    const url1 = `///https://www.alphavantage.co/query?function=OVERVIEW&symbol=${input.tickerSymbol}&apikey=5MRPGIK2APTZ9BFT`;
-    // axios request for company information
-    axios
-      .get(url1)
-      .then((res) => {
-        //console.log("First request: ", res);
-        setStock({
-          ticker: res.data.Symbol,
-          type: res.data.AssetType,
-          name: res.data.Name,
-          exchange: res.data.Exchange,
-          country: res.data.Country,
-          sector: res.data.Sector,
-          industry: res.data.Industry,
-          fiftytwoh: res.data.WeekHigh,
-          fiftytwol: res.data.WeekLow
-        });
-        //console.log(stock);
-        //console.log(stock.ticker);
-      })
-      .catch((err) => {
-        console.log("Error calling URL1: ", err);
-      });
-    // axios request for stock price
-    const url2 =
-      "///https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol=IBM&apikey=5MRPGIK2APTZ9BFT";
+  // useEffect(() => {
+  //   const url1 = `///https://www.alphavantage.co/query?function=OVERVIEW&symbol=${input.tickerSymbol}&apikey=5MRPGIK2APTZ9BFT`;
+  //   // axios request for company information
+  //   axios
+  //     .get(url1)
+  //     .then((res) => {
+  //       //console.log("First request: ", res);
+  //       setStock({
+  //         ticker: res.data.Symbol,
+  //         type: res.data.AssetType,
+  //         name: res.data.Name,
+  //         exchange: res.data.Exchange,
+  //         country: res.data.Country,
+  //         sector: res.data.Sector,
+  //         industry: res.data.Industry,
+  //         fiftytwoh: res.data.WeekHigh,
+  //         fiftytwol: res.data.WeekLow
+  //       });
+  //     })
+  //     .catch((err) => {
+  //       console.log("Error calling URL1: ", err);
+  //     });
+  //   //
+  //   // axios request for stock price
+  //   const url2 =
+  //     "///https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol=IBM&apikey=5MRPGIK2APTZ9BFT";
 
-    axios
-      .get(url2)
-      .then((res) => {
-        console.log(
-          "Second request: ",
-          res.data["Time Series (Daily)"]["2020-08-11"]
-        );
-        setPrice({
-          open: res.data["Time Series (Daily)"]["2020-08-11"]["1. open"],
-          high: res.data["Time Series (Daily)"]["2020-08-11"]["2. high"],
-          low: res.data["Time Series (Daily)"]["2020-08-11"]["3. low"],
-          close: res.data["Time Series (Daily)"]["2020-08-11"]["4. close"],
-          volume: res.data["Time Series (Daily)"]["2020-08-11"]["5. volume"]
-        });
-      })
-      .catch((err) => {
-        console.log("Error calling URL2: ", err);
-      });
-
-    // const submit = (x) => {
-    //   setInput({ tickerSymbol: x });
-    // };
-
-    //submit("hi");
-  }, []);
+  //   axios
+  //     .get(url2)
+  //     .then((res) => {
+  //       console.log(
+  //         "Second request: ",
+  //         res.data["Time Series (Daily)"]["2020-08-11"]
+  //       );
+  //       setPrice({
+  //         open: res.data["Time Series (Daily)"]["2020-08-11"]["1. open"],
+  //         high: res.data["Time Series (Daily)"]["2020-08-11"]["2. high"],
+  //         low: res.data["Time Series (Daily)"]["2020-08-11"]["3. low"],
+  //         close: res.data["Time Series (Daily)"]["2020-08-11"]["4. close"],
+  //         volume: res.data["Time Series (Daily)"]["2020-08-11"]["5. volume"]
+  //       });
+  //     })
+  //     .catch((err) => {
+  //       console.log("Error calling URL2: ", err);
+  //     });
+  // }, []);
 
   const submit = (e) => {
     e.preventDefault();
   };
 
-  const url3 = `https://www.alphavantage.co/query?function=OVERVIEW&symbol=${input.tickerSymbol}&apikey=5MRPGIK2APTZ9BFT`;
+  const url3 = `//https://www.alphavantage.co/query?function=OVERVIEW&symbol=${input.tickerSymbol}&apikey=5MRPGIK2APTZ9BFT`;
 
   const refresh = (e) => {
     axios.get(url3).then((res) => {
       console.log(res.data);
+      setStock({
+        ticker: res.data.Symbol,
+        type: res.data.AssetType,
+        name: res.data.Name,
+        exchange: res.data.Exchange,
+        country: res.data.Country,
+        sector: res.data.Sector,
+        industry: res.data.Industry,
+        fiftytwoh: res.data.WeekHigh,
+        fiftytwol: res.data.WeekLow
+      });
     });
   };
+
+  useEffect(() => {
+    refresh();
+  });
 
   return (
     <div className="App">
